62f74dd4e796f203918ce2b0dc261a86e94b8285
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2021-04-09 17:13:01 -0300

    Modificado fichacadastral.php para o novo formato de importação

--- app/public/Views/home.php
+++ app/public/Views/home.php
@@ -2,7 +2,7 @@
 
 <!-- Content Section -->
 <div class="content col-md-12">
-    <h1>Content</h1>
+    <h1><a class="nav-link" href="public/Views/fichacadastral.php">Ficha Cadastral<span class="sr-only">(current)</span></a></h1>
     <h1>Content</h1>
     <h1>Content</h1>
     <h1>Content</h1>
