af208683dfe9013da0cd323fd9eddc5e4d573da7
Ulisses <ulisses.skysect@hotmail.com>
2021-04-11 16:28:00 -0300

    Update questgrowkid.php and custom.css // Create checkpront.php

---
+++ app/public/Views/checkpront.php
@@ -0,0 +1,21 @@
+<?php require_once 'layouts/base.php'; ?>
+
+<!-- Content Section -->
+<div class="content col-md-12">
+    
+    <form action="post">
+        <div class="containerpront">
+
+            <h4 class="exibetitulo">Prontuario de Atendimento<h4>
+            <label class="textformat">Data da consulta:</label><br>
+            InfoBanco <br>
+
+            <label class="textformat">Os procedimentos realizados nesta data foram:</label><br>
+            InfoBanco
+        </div>
+    </form>
+
+</div>
+
+<!-- Includes footer and close html -->
+<?php require_once 'layouts/footer.html'; ?>
