9c87c662977a4d020cc58b83ac74397c41171324
Ulisses <ulisses.skysect@hotmail.com>
2021-04-11 02:51:22 -0300

    Criacao ficha prontuario

---
+++ app/public/Views/prontuario.php
@@ -0,0 +1,328 @@
+<?php require_once 'layouts/base.php'; ?>
+
+<!-- Content Section -->
+<div class="content col-md-12">
+
+    <div>
+
+        <h1 class="exibetitulo">Prontuário</h1>
+        <div class="col-md-6">
+            <div class="controlcontainer firstcont">
+                <h4>Questionário de Saúde</h4>
+                <b>Queixa Principal</b><br>
+                InfoBanco
+                <br>
+
+                <b>Histórico de Tratamento Atual</b><br>
+                InfoBanco
+                <br>
+
+                <b>1- Teve Hemorragiba?</b><br>
+                InfoBanco
+                <br>
+
+                <b>2- Sofre/sofreu alguma alergia?</b><br>
+                InfoBanco
+                <br>
+
+                <b>3- Teve reumatisco infeccioso?</b><br>
+                InfoBanco
+                <br>
+
+                <b>4- Sofre(u) de distúrbio cardiovascular?</b><br>
+                InfoBanco
+                <br>
+
+                <b>5- Sofre(u) de gastrite?</b><br>
+                InfoBanco
+                <br>
+
+                <b>6- É diabético ou tem familiares diabéticos?</b><br>
+                InfoBanco
+                <br>
+
+                <b>7- Já desmaiou alguma vez?</b><br>
+                InfoBanco
+                <br>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="controlcontainer firstcont"> 
+
+                <b>8- Está sob tratamento médico?</b><br>
+                InfoBanco
+                <br>
+
+                <b>9- Está tomando algum medicamento?</b><br>
+                InfoBanco
+                <br>
+
+                <b>10- Esteve doente ou foi operado nos últimos 5 anos?</b><br>
+                InfoBanco
+                <br>
+
+                <b>11- Tem hábitos, vícios ou manias?</b><br>
+                InfoBanco
+                <br>
+
+                <b>12- Tem ansiedade/depressão?</b><br>
+                InfoBanco
+                <br>
+
+                <b>13 -	Você e/ou algum familiar  teve algumas dessas doenças?</b><br>
+                InfoBanco
+                <br>
+
+                <div class="col-md-6">
+                    <b>Tuberculose</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Sífilis</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Hepatite A, B, C</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>SIDA/AIDS</b><br>
+                    InfoBanco
+                    <br>
+                </div>
+
+                <div class="col-md-6">
+                    <b>Sarampo</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Caxumba</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Varicela</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Outras</b><br>
+                    InfoBanco
+                    <br>
+                </div>
+                <br>
+
+                <b>14-	É fumante?</b><br>
+                InfoBanco  <b>Com que frequência?</b> InfoBanco <b>/dia</b><br>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="controlcontainer lastcont">
+                <h4 class="exibetitulo">Questionário Infantil</h4><br>
+
+                <b>Histórico Gestacional</b><br>
+                InfoBanco
+                <br>
+
+                <b>Nasceu de Parto</b><br>
+                InfoBanco
+                <br>
+
+                <b>Problemas Durante o Parto?</b><br>
+                InfoBanco
+                <br>
+
+                <b>A amamentação foi:</b><br>
+                InfoBanco <b>até a idade de:</b> InfoBanco <b>anos</b><br>
+                <br>
+
+                <b>Já lhe foi dito para não tomar anestesia local?</b><br>
+                InfoBanco
+                <br>
+
+                <b>Já teve ou viveu com alguém que tivesse doença grave e contagiosa?</b><br>
+                InfoBanco
+                <br>
+
+                <b>A criança já foi vacinada?</b><br>
+                InfoBanco
+                <br>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="controlcontainer lastcont"> 
+                <h4 class="exibetitulo">Conduta da Criança</h4>
+
+                <b>Durante os primeiros 2 anos de vida a criança:</b><br>
+                    <b>Sentou-se</b> InfoBanco <br>
+                    <b>Engatinhou</b> InfoBanco <br>
+                    <b>Andou</b> InfoBanco <br>
+                    <b>Falou</b> InfoBanco <br>
+                <br>
+
+                <b>No lar e na escola a criança apresentou alguma dificuldade no aprendizado?</b><br>
+                InfoBanco
+                <br>
+
+                <b>Estado Anímico:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Tem o sono:</b><br>
+                InfoBanco
+                <br>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="col-md-6">
+                <div class="controlcontainer lastcont"> 
+                    <h4 class="exibetitulo">Apresenta Alguma Patologia de Conduta:</h4>
+                    <b>Ansiedade:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Medo:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Birra:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Ciúme:</b><br>
+                    InfoBanco
+                    <br>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="controlcontainer lastcont"> 
+                    <h4 class="exibetitulo">Conduta Psicomotora:</h4>
+                    <b>Postura:</b> InfoBanco <br>
+                    <b>Fala:</b> InfoBanco <br>
+                    <b>Alguma Paralisia:</b> InfoBanco <br>
+                    <b>Enurese Noturna:</b> InfoBanco <br>
+                    <b>Descontrole dos Esfíncteres</b> InfoBanco <br>
+                    <br>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="controlcontainer lastcont"> 
+                    <h4 class="exibetitulo">Alimentação</h4>  
+                    InfoBanco
+                </div>
+            </div>
+        </div> 
+
+        <div class="col-md-6">
+            <div class="controlcontainer lastcont"> 
+                <h4 class="exibetitulo">Exame Físico</h4>
+                <div class="col-md-6">
+
+                    <b>Lábio:</b><br>
+                    InfoBanco
+                    <br>
+                    
+                    <b>Mucosa Jugal:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Língua:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Soalho da Boca:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Paleto Duro:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Garganta:</b><br>
+                    InfoBanco
+                    <br>
+
+                    <b>Paleto Mole:</b><br>
+                    InfoBanco
+                    <br>
+
+                </div>
+                <b>Mucosa Alveolar:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Gengivas:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Glândulas Salivares:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Linfonodos:</b><br>
+                InfoBanco
+                <br>
+
+                <b>ATM:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Músculos Mastigadores:</b><br>
+                InfoBanco
+                <br>
+
+                <b>Oclusão:</b><br>
+                InfoBanco
+                <br>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="controlcontainer lastcont">
+                <h4 class="exibetitulo">Desfecho de Consulta:</h4>
+                <b>Alterações Encontradas:</b>InfoBanco<br>
+                <br>
+
+                <b>Pressão Arterial Máxima:</b>InfoBanco <b>mmgH</b><br>
+                <br>
+
+                <b>Pressão Arterial Mínima:</b>InfoBanco <b>mmgH</b><br>
+                <br>
+
+                <b>Diagnóstico Presuntivo:</b>InfoBanco<br>
+                <br>
+
+                <b>Exames Complementares:</b>InfoBanco<br>
+                <br>
+
+                <b>Diagnóstico Definitivo:</b>InfoBanco<br>
+                <br>
+
+                <b>Tratamento/Proservação:</b>InfoBanco<br>
+                <br>
+
+                <b>Plano de Tratamento:</b>InfoBanco<br>
+                <br>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="controlcontainer lastcont"> 
+                <h4 class="exibetitulo">Responsáveis do Tratamento</h4>
+                <b>Aluno:</b><br>        
+                InfoBanco
+                <br>
+                <b>Professor:</b><br>
+                InfoBanco
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Includes footer and close html -->
+<?php require_once 'layouts/footer.html'; ?>
