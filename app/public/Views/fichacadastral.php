cfa6ec08fd91c8a483bdf04cad7df69dfc12890b
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2021-04-12 20:34:09 -0300

    adicionado api viacep na pagina de cadastro de paciente

--- app/public/Views/fichacadastral.php
+++ app/public/Views/fichacadastral.php
@@ -1,5 +1,75 @@
 <?php require_once 'layouts/base.php'; ?>
 
+ <!-- Adicionando Javascript -->
+ <script>
+    
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade_endereco').value=("");
+            document.getElementById('estado_endereco').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade_endereco').value=(conteudo.localidade);
+            document.getElementById('estado_endereco').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade_endereco').value="...";
+                document.getElementById('estado_endereco').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    </script>
+
 <!-- Content Section -->
 <div class="content col-md-12">
     <form method="post" action="/Http/Controller/PacienteController.php?acao=inserir">
@@ -202,18 +272,18 @@
 
             <div class="label-float">
                 <input type="text" 
-                type="text" 
+                onblur="pesquisacep(this.value)"; 
                 name="CEP" 
                 id="CEP" 
                 placeholder="Cep"
+                maxlength="9"
                 required
                 />
                 <label>Digite seu CEP:</label>
             </div>
 
             <div class="label-float">
-                <input type="text" 
-                type="text" 
+                <input type="text"  
                 name="rua" 
                 id="rua" 
                 placeholder="Endereço"
